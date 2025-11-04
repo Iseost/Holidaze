e90be72a64c7236e4018f9fecf43d881a476821d
Iselin Engen <iselin.osterhus@gmail.com>
2025-11-04 03:37:38 +0100

    Update api
    Add API constants and initial Auth and Venues modules

---
+++ holidaze/src/api/constants.mjs
@@ -0,0 +1,6 @@
+export const API_KEY = "106a7b8a-36b1-497d-94ec-5c7d9f60aca2";
+export const API_BASE = "https://v2.api.noroff.dev";
+export const API_AUTH = `${API_BASE}/auth`;
+export const API_VENUES = `${API_BASE}/holidaze/venues`;
+export const API_BOOKINGS = `${API_BASE}/holidaze/bookings`;
+export const API_USERS = `${API_BASE}/holidaze/users`;
