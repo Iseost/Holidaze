eee3a118f6ad090191c437d88a06b285dd0a4c77
Iselin Engen <iselin.osterhus@gmail.com>
2025-11-18 09:55:32 +0100

    update profile api and profile page

---
+++ holidaze/src/pages/ProfileCustomer.jsx
@@ -0,0 +1,7 @@
+import { useEffect, useState } from "react";
+import { getUserProfileWithBookings, updateAvatar, updateBanner } from "../api/profiles.mjs";
+
+export default function ProfileCustomer() {
+    const [profile, setProfile] = useState(null);
+    const []
+} 
